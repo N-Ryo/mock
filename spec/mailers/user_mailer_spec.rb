bd33fa09c1b8e9233605fdcccd26c4211e7b8397
N-Ryo <ryo.joy.joy@gmail.com>
2019-02-08 02:32:03 +0900

    Add account activation

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,19 +1,24 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+let(:user) { FactoryBot.create(:user) }
+=begin
   describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
+    let(:mail) { UserMailer.account_activation(user) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq(user.email)
+      expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match user.name
+      expect(mail.body.encoded).to match user.activation_token
+      expect(mail.body.encoded).to match CGI.escape(user.email)
     end
   end
+=end
 
   describe "password_reset" do
     let(:mail) { UserMailer.password_reset }
@@ -21,7 +26,7 @@ RSpec.describe UserMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Password reset")
       expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do
