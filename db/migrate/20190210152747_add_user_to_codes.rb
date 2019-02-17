85895a6ce4fc7dcb9f380e34593ec3ff9b4940f5
N-Ryo <ryo.joy.joy@gmail.com>
2019-02-17 19:44:40 +0900

    create hack-tag system

--- db/migrate/20190210152747_add_user_to_codes.rb
+++ db/migrate/20190210152747_add_user_to_codes.rb
@@ -3,5 +3,4 @@ class AddUserToCodes < ActiveRecord::Migration[5.1]
     add_reference :codes, :hack, foreign_key: true
     add_reference :codes, :user, foreign_key: true 
   end
-  add_index :codes, [:user_id, :created_at]
 end
