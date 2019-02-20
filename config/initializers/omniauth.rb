b8b4e50524446aa69111d52c52ef004b0a7acc59
N-Ryo <ryo.joy.joy@gmail.com>
2019-02-20 10:36:12 +0900

    change-welcome-sign

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :slack, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], scope: "client"
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-    provider :twitter, "API_KEY", "API_SECRET"
-end
