72a28763ba3daf6ba512cee887b2ab2596c706ed
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-15 15:33:02 +0900

    Add seach result and Modify css

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,4 +1,16 @@
 class SearchsController < ApplicationController
   def index
+    @categories = Category.where(name: params[:key_word])
+    @roles = Role.where(name: params[:key_word])
+    @category_hacks = []
+    @role_hacks = []
+    @categories.each do |category|
+      @category_hacks.push(Hack.where(category_id: category.id))
+    end
+    @roles.each do |role|
+      @role_hacks.push(Hack.where(role_id: role.id))
+    end
+    @category_hacks.flatten!
+    @role_hacks.flatten!
   end
 end
