1c725156ec1d19d676be4e747d015ca8428abdca
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-27 15:12:46 +0900

    Fix heroku-app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'powerful-refuge-32827.herokuapp.com'
+  host = 'move-hack.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
